4734ee1bf9775ff4d79bd7945bc630eb34412145
Rezzy <74584623+srcfur@users.noreply.github.com>
2026-08-27 01:55:29 -0400

    Init

---
+++ common/src/main/java/com/srcfur/puppycraft/Constants.java
@@ -0,0 +1,11 @@
+package com.srcfur.puppycraft;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Constants {
+
+	public static final String MOD_ID = "puppycraft";
+	public static final String MOD_NAME = "Puppy Craft";
+	public static final Logger LOG = LoggerFactory.getLogger(MOD_NAME);
+}
